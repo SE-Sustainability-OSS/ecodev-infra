12a72ce5fafd7c9ac87135c9ea99a5f6adf26f0b
Thomas Epelbaum <107172490+tepelbaum@users.noreply.github.com>
2024-10-01 11:25:35 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@
 
 echo "Installing docker"
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg make
+sudo apt-get install -y -qq ca-certificates curl gnupg make
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Add access rights for current user" $1
 sudo usermod -a -G docker $1
